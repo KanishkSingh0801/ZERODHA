604bee47393d798ca4b71f933cfbf94b964dde98
KanishkSingh0801 <kanishksinghjatav@gmail.com>
2025-06-03 15:59:53 +0530

    Backend setup

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express(); //For creating a new application
+
+
+app.listen(3002, () => {
+    console.log("Backend server is running on port 3002");
+})
+
